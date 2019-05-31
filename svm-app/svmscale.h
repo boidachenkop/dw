2b2152d219bb373dc583ea9e4cedb56c4947c7e6
Pavlo Boidachenko <boidachenkopavlo@gmail.com>
2019-05-31 17:14:26 +0200

    Implemented svmscale class. No test yet. No connection with ui

--- svm-app/svmscale.h
+++ svm-app/svmscale.h
@@ -5,33 +5,58 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <string>
 #include <algorithm>
+
+#define SKIP_TARGET\
+    while(isspace(*p)) ++p;\
+    while(!isspace(*p)) ++p;
+
+#define SKIP_ELEMENT\
+    while(*p!=':') ++p;\
+    ++p;\
+    while(isspace(*p)) ++p;\
+    while(*p && !isspace(*p)) ++p;
 
 class svmscale
 {
 public:
-    svmscale();
+    svmscale(std::string filepath);
+    bool check();
+    void scale();
+
+    //setters
+    void setLowerUpper(double lower, double upper);
+    void setYLowerUpper(double lower, double upper);
+    void setRestoreFilepath(char* range_filepath);
 
 private:
     double lower{-1.0};
     double upper{1.0};
+    double *feature_max, *feature_min;
 
-    int y_scaling{0};
+    bool y_scaling{false};
     double y_lower, y_upper;
-    double y_max = -DBL_MAX;
-    double y_min = DBL_MAX;
+    double y_max{-DBL_MAX};
+    double y_min{DBL_MAX};
+
+    int index;
     int max_index, min_index;
 
-    long int num_nonzeros = 0;
-    long int new_num_nonzeros = 0;
+    long int num_nonzeros{0};
+    long int new_num_nonzeros{0};
 
-    FILE *fp, *fp_restore = NULL;
-    char *save_filename = NULL;
-    char *restore_filename = NULL;
+    FILE *fp, *fp_restore{nullptr};
+    char *save_filename{nullptr};
+    char *restore_filename{nullptr};
+    std::string input_filepath;
+
+    char *line{nullptr};
+    int max_line_len{1024};
 private:
+    char* readline(FILE *input);
     void output_target(double value);
     void output(int index, double value);
-    char* readline(FILE *input);
     int clean_up(FILE *fp_restore, FILE *fp, const char *msg);
 };
 
