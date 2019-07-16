40809af9961cbe19eaf91e7d46051bcc8813f4bd
PAVLO BOIDACHENKO <pavlo.boidachenko@comarch.com>
2019-07-16 14:14:05 +0200

    Changed window title to 'SVM App'

--- svm-app/main.cpp
+++ svm-app/main.cpp
@@ -6,11 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowTitle("SVM App");
     w.show();
-    FILE* file;
-    file = fopen("tstfile.txt", "w");
-    double dbl = 0.5876;
-    fprintf(file, "%.17f", dbl);
-    fclose(file);
     return a.exec();
 }
